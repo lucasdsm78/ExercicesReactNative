dbbdbbf527d9cf0ae01159e5c2f86d1b36b0d3c4
lucasdsm78 <66834518+lucasdsm78@users.noreply.github.com>
2021-12-17 21:26:19 +0100

    ListRecettes

---
+++ App.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  FlatList,
+  useColorScheme,
+  View,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const DATA = [
+  {
+    title: 'Solène',
+  },
+  {
+    title: 'Léo',
+  },
+  {
+    title: 'Francesco',
+  },
+  {
+    title: 'Marine',
+  },
+  {
+    title: 'Lucas',
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={styles.item}>
+  <TouchableOpacity style={styles.touchStyle}>
+    <Text onLongPress={() => alert("Bonjour " + title)}> {title} </Text>
+  </TouchableOpacity>
+    </View>
+);
+
+const App = () => {
+  const renderItem = ({ item }) => (
+    <Item title={item.title} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+
+
+export default App;
